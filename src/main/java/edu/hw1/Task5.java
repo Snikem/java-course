e3318f9dbbf6d5a41ac08ad892241b73c8fa6e43
anton glukhikh <antong.21072003@gmail.com>
2023-10-07 21:42:51 +0300

    task 5-6

--- src/main/java/edu/hw1/Task5.java
+++ src/main/java/edu/hw1/Task5.java
@@ -3,8 +3,51 @@ package edu.hw1;
 public class Task5 {
 
 
+    public static boolean isPalindrome(int n)
+    {
+       String str_n = String.valueOf(n);
+       StringBuilder sb =new StringBuilder(str_n);
+       sb.reverse();
+       String str_n_reverse = sb.toString();
+       return str_n_reverse.equals(str_n);
+    }
+    public static int toDescendant(int n)
+    {
+        int result = 0;
+        int i = 0;
+        while (n != 0)
+        {
+            result +=(n % 10 + (n % 100 / 10)) * Math.pow(10,i);
+            n/=100;
+            i++;
+        }
+        return result;
+    }
+
+    public static boolean isPalindromeDescendantRecurse(int n)
+    {
+        if(String.valueOf(n).length() <= 1)
+        {
+            return false;
+        }
+        if(isPalindrome(n))
+        {
+            return true;
+        }
+        return isPalindromeDescendantRecurse(toDescendant(n));
+
+    }
+
+    public static boolean isPalindromeDescendant(int n)
+    {
+        if(String.valueOf(n).length() % 2 == 1)
+        {
+            n*=10;
+        }
+        return isPalindromeDescendantRecurse(n);
+    }
     public static void main(String[] args)
     {
-
+        System.out.println(isPalindromeDescendant(220));
     }
 }
--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -1,4 +1,34 @@
 package edu.hw1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task6 {
+
+    public static int countK(int n, int count)
+    {
+        if(n == 6174)
+        {
+            return count;
+        }
+        int[] arr = new int[]{n / 1000,n % 1000 / 100,n % 100 / 10, n % 10};
+        Arrays.sort(arr);
+        int toMin = 0;
+        int toMax = 0;
+        for(int i = 0; i < 4;i++)
+        {
+
+            toMax+= Math.pow(10,i) * arr[i];
+            toMin+= Math.pow(10,3 - i) * arr[i];
+        }
+        System.out.println(toMax - toMin);
+        count++;
+        return countK(toMax - toMin,count);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(countK(1234,0));
+
+    }
 }
